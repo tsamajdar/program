3ad98210b2a5efcec88373c3ca6645b8c52ac8ab
tsamajdar <52111858+tsamajdar@users.noreply.github.com>
2020-04-27 22:11:15 -0700

    Update sum.java

--- sum.java
+++ sum.java
@@ -5,9 +5,11 @@ public class sum
     System.out.println("The number is "+n);
     int i=0,c=0;
     for(i=1;i<=n;i++)
-    {
+    {    
         c=c+i;
-        System.out.println(c);
     }
+System.out.println("The sum of numbers is "+c);
+
+
 }
 }
