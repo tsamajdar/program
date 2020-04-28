31e44ab545a2270fb58c1f15ad6e58594ef79d39
tsamajdar <52111858+tsamajdar@users.noreply.github.com>
2020-04-27 22:00:43 -0700

    summation of n numbers in java to run in  Blue J

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum
+{
+    public static void main(int n)
+    {
+    System.out.println("The number is "+n);
+    int i=0,c=0;
+    for(i=1;i<=n;i++)
+    {
+        c=c+i;
+        System.out.println(c);
+    }
+}
+}
